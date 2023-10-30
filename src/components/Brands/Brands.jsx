5b15474e4f352ae1d4f40213f7a357c71545038d
Emin 14 <emin-agjaev@mail.ru>
2023-10-30 23:04:10 +0300

    used REM in titles

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -7,12 +7,15 @@ import image31 from '../../assets/img/Brands/image31.svg';
 
 export default function Brands() {
   return (
-    <div className={styles.wrapper}>
-      <img src={image27} alt="" />
-      <img src={image28} alt="" />
-      <img src={image29} alt="" />
-      <img src={image30} alt="" />
-      <img src={image31} alt="" />
+    <div className='container'>
+      <div className={styles.wrapper}>
+        <img src={image27} alt="" />
+        <img src={image28} alt="" />
+        <img src={image29} alt="" />
+        <img src={image30} alt="" />
+        <img src={image31} alt="" />
+      </div>
     </div>
+
   );
 }
--- src/components/Destinations/Destinations.jsx
+++ src/components/Destinations/Destinations.jsx
@@ -13,37 +13,40 @@ export default function Destinations() {
   const data = t('destinations.data', { returnObjects: true });
 
   return (
-    <div className={styles.destinations}>
-      <h5 className={styles.subtitle}>{t('destinations.subtitle')}</h5>
-      <h3 className={styles.title}>{t('destinations.title')}</h3>
-      <ul className={styles.list}>
-        {data.map((item) => (
-          <li key={item.id} className={styles.item}>
-            <div className={styles.imageWrapper}>
-              <img className={styles.img} src={img[item.id]} alt="" />
-            </div>
-            <div className={styles.bottom}>
-              <div className={styles.placePrice}>
-                <span className={styles.place}>{item.place}</span>
-                <span className={styles.price}>
-                  $
-                  {item.price}
-                  k
-                </span>
+    <div className='container'>
+      <div className={styles.destinations}>
+        <h5 className={styles.subtitle}>{t('destinations.subtitle')}</h5>
+        <h3 className={styles.title}>{t('destinations.title')}</h3>
+        <ul className={styles.list}>
+          {data.map((item) => (
+            <li key={item.id} className={styles.item}>
+              <div className={styles.imageWrapper}>
+                <img className={styles.img} src={img[item.id]} alt="" />
               </div>
-              <div className={styles.days}>
-                <img className={styles.icon} src={navigation} alt="" />
-                <span className={styles.description}>
-                  {item.days}
-                  {' '}
-                  {t('destinations.days')}
-                </span>
+              <div className={styles.bottom}>
+                <div className={styles.placePrice}>
+                  <span className={styles.place}>{item.place}</span>
+                  <span className={styles.price}>
+                    $
+                    {item.price}
+                    k
+                  </span>
+                </div>
+                <div className={styles.days}>
+                  <img className={styles.icon} src={navigation} alt="" />
+                  <span className={styles.description}>
+                    {item.days}
+                    {' '}
+                    {t('destinations.days')}
+                  </span>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <img src={decore} className={styles.decore} alt="decore" />
+            </li>
+          ))}
+            {/* <img src={decore} className={styles.decore} alt="decore" /> */}
+          </ul>
+      </div>
     </div>
+    
   );
 }
